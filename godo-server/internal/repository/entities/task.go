7a55ae2271940932822d49ba322385f65e906400
Michael Murphy <michael.w.murphy@outlook.com>
2022-07-14 10:11:17 +0100

    Fixes Task gorm config

--- godo-server/internal/repository/entities/task.go
+++ godo-server/internal/repository/entities/task.go
@@ -13,8 +13,8 @@ type Task struct {
 	Story       Story     `json:"-"`
 	Name        string    `json:"name" gorm:"not null" validate:"required,min=1,max=40"`
 	Description string    `json:"description" validate:"max=280"`
-	Type        int       `json:"type" gorm:"default:0,not null"`
-	Status      int       `json:"status" gorm:"default:0,not null"`
+	Type        int       `json:"type" gorm:"not null,default:0"`
+	Status      int       `json:"status" gorm:"not null,default:0"`
 	CreatorId   string    `json:"-"`
 	Creator     auth.User `gorm:"foreignKey:CreatorId"`
 
