26cf178a7877d7914c3305d7db15a41691c81714
Michael Murphy <michael.w.murphy@outlook.com>
2022-07-27 14:52:37 +0100

    Adds descriptive comment

--- godo-server/internal/repository/entities/tag.go
+++ godo-server/internal/repository/entities/tag.go
@@ -1,5 +1,8 @@
 package entities
 
+// Tag - A Project has any number of tags associated with it
+// A subset of these tgs can be assigned to any of the tasks
+// that form part of the Project
 type Tag struct {
 	ID        uint    `json:"id" gorm:"primary_key"`
 	Name      string  `json:"name"`
